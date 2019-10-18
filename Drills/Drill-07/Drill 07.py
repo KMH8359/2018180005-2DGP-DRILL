ac8b24fd792e6aa18804ae2fd65c5751f985c2d3
KMH8359 <50608712+KMH8359@users.noreply.github.com>
2019-10-18 11:53:35 +0900

    Drill 7 진행
    기본

---
+++ Drills/Drill-07/Drill 07.py
@@ -0,0 +1,53 @@
+from pico2d import *
+import random
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+class Boy:
+    def init(self):
+        self.x = random.randint(100, 700)
+        self.y = 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+    def update(self):
+        self.x += 5
+        self.frame = (self.frame + 1) % 8
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+class Grass:
+    def init(self):
+        self.image = load_image('grass.png')
+    def draw(self):
+        self.image.draw(400, 30)
+
+boy = Boy()
+grass = Grass()
+team = [Boy() for i in range(11)]
+
+open_canvas()
+
+running = True
+for boy in team:
+    boy.init()
+
+grass.init()
+while running:
+    handle_events()
+    for boy in team:
+        boy.update()
+
+    clear_canvas()
+    grass.draw()
+    for boy in team:
+        boy.draw()
+    update_canvas()
+    delay(0.05)
+close_canvas()
